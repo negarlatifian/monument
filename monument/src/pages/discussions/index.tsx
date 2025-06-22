5249766740dbba7de1fd1c23879c62d26a149e51
neela-platform <74406918+negarlatifian@users.noreply.github.com>
2025-06-22 23:24:44 +0300

    discussion mvp done

---
+++ monument/src/pages/discussions/index.tsx
@@ -0,0 +1,19 @@
+// src/pages/theproject.tsx
+import Head from 'next/head';
+import type { NextPage } from 'next';
+
+const DiscussionsPage: NextPage = () => (
+  <>
+    <Head>
+      <title>The Project Svenska | Monument of the City to Build</title>
+      <meta name='Discussions Page' content='Disscussions page' />
+    </Head>
+    <div className='flex flex-col ml-5 mt-15 mr-15 mb-20'>
+      <section className='flex flex-row text-[1.3rem] font-semibold leading-5 gap-20 mb-15 h-[55vh] justify-center '>
+        <p>Coming Soon</p>
+      </section>
+    </div>
+  </>
+);
+
+export default DiscussionsPage;
